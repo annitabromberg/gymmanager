942ef6250016feb05555de879afcc043b686b37e
Anna Bromberg <annita.bromberg@gmail.com>
2026-07-06 19:56:37 -0300

    publicacaion en https://railway.com/

--- app.py
+++ app.py
@@ -667,5 +667,5 @@ def perfil_alumno(id):
 # ===============================
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
     
